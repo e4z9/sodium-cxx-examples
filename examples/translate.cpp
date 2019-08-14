96451e738c6dc9434da8eaa67b55f88234109489
Eike Ziller <git@eikeziller.de>
2019-08-14 17:12:39 +0200

    Add translate example

---
+++ examples/translate.cpp
@@ -0,0 +1,42 @@
+#include <sqlabel.h>
+#include <sqlineedit.h>
+#include <sqpushbutton.h>
+
+#include <sodium/sodium.h>
+
+#include <QApplication>
+#include <QLabel>
+#include <QRegularExpression>
+#include <QVBoxLayout>
+#include <QWidget>
+
+using namespace sodium;
+
+// Chapter 2.7 Listing 2.6
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    auto english = new SQLineEdit("I like FRP");
+    auto translate = new SQPushButton("Translate");
+    stream<QString> sLatin = translate->sClicked
+                                 .snapshot(english->text, [](unit, const QString &text) {
+                                     QString result = text.trimmed();
+                                     result.replace(QRegularExpression("\\s+|$"), "us ");
+                                     return result.trimmed();
+                                 });
+    cell<QString> latin = sLatin.hold("");
+    auto lblLatin = new SQLabel(latin);
+
+    // GUI layout
+    QWidget window;
+    auto layout = new QVBoxLayout;
+    window.setLayout(layout);
+    layout->addWidget(english);
+    layout->addWidget(translate);
+    layout->addWidget(lblLatin);
+    layout->addStretch();
+    window.setGeometry(100, 100, 500, 500);
+    window.show();
+    return app.exec();
+}
